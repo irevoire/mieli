dc2da8743fee9383fffd61527ce56cfa74524207
Tamo <tamo@meilisearch.com>
2025-01-15 21:13:29 +0100

    improve the error reporting of the meilisearch response

--- src/format.rs
+++ src/format.rs
@@ -1,20 +1,46 @@
-use miette::{IntoDiagnostic, Result};
+use miette::{Context, IntoDiagnostic, Result};
 use reqwest::blocking::Response;
 use serde_json::Value;
 use termion::color;
 
 pub fn write_response_headers(response: &Response, verbose: u8) -> Result<()> {
-    if verbose < 1 {
+    let status = response.status();
+    if verbose < 1 && status.is_success() {
         return Ok(());
     }
     let colored = atty::is(atty::Stream::Stderr);
 
+    // HTTP/1.1 404 Not Found
+    if colored {
+        eprint!("{}", color::Fg(color::Blue));
+    }
+    eprint!("{:?} {:?}", response.version(), status);
     if colored {
         eprint!("{}", color::Fg(color::Cyan));
     }
+    if let Some(reason) = status.canonical_reason() {
+        eprint!(" {reason}");
+    }
+    eprintln!();
+
+    let mut headers: Vec<_> = response.headers().iter().collect();
+    headers.sort_by_key(|(k, _)| k.as_str());
 
     for (key, value) in response.headers() {
-        eprintln!("{}: {:?}", key, value);
+        if colored {
+            eprint!("{}", color::Fg(color::Cyan));
+        }
+
+        eprint!("{key}:");
+
+        if colored {
+            eprint!("{}", color::Fg(color::White));
+        }
+        if let Ok(value) = value.to_str() {
+            eprintln!(" {value}");
+        } else {
+            eprintln!(" {value:?}");
+        }
     }
 
     if colored {
@@ -40,5 +66,15 @@ pub fn write_json(response: Value) -> Result<Value> {
 
 pub fn write_response_full(response: Response, verbose: u8) -> Result<Value> {
     write_response_headers(&response, verbose)?;
-    write_json(response.json().into_diagnostic()?)
+    let body = response
+        .bytes()
+        .into_diagnostic()
+        .context("While retrieving the body as bytes")?;
+    if body.is_empty() {
+        return Ok(serde_json::Value::Null);
+    }
+    let json: serde_json::Value = serde_json::from_slice(&body)
+        .into_diagnostic()
+        .context(format!("While converting the body as json: {body:?}"))?;
+    write_json(json)
 }
